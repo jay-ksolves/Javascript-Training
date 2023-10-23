ea7de91d75e43e214568bab35bb49a7445977bee
jay-ksloves <jay.prakash@ksolves.com>
2023-10-23 18:50:17 +0530

    done promises in js

--- learning_calbacks/callback.js
+++ learning_calbacks/callback.js
@@ -14,37 +14,86 @@ function show_details(c, d, callback) {
     console.log(`My last name is ${d}`);
     callback(c, d);
 }
-//calling 2nd function and bassing the attributes also first function as an argument ------------------
+// calling 2nd function and passing the attributes and the first function as an argument ------------------
 show_details(First_name, Last_name, show_name);
 
-//======================Nested callback in js====================================================
+// ======================Nested callback in js====================================================
 console.log("");
 console.log("-------This output for nested callback---------------");
 
 function function1(callback) {
-    console.log("Task 1 executed..!.");
-    callback();
+    setTimeout(function () {
+        console.log("function 1 executed..!.");
+        callback();
+    }, 1000);
 }
 
 function function2(callback) {
-    console.log("Task 2 executed..!.");
-    callback();
+    setTimeout(function () {
+        console.log("function 2 executed..!.");
+        callback();
+    }, 1000);
 }
 
 function function3(callback) {
-    console.log("Task 3 executed..!.");
-    callback();
+    setTimeout(function () {
+        console.log("function 3 executed..!.");
+        callback();
+
+    }, 1000);
 }
 
-function startTasks() {
+function startFunctions() {
     function1(function () {
         function2(function () {
             function3(function () {
-                console.log("All tasks executed..!.");
+                console.log("All functions executed..!.");
             });
         });
     });
 }
 
-startTasks();
+startFunctions();
 
+// =====================Promises (handling callback hell)=======================
+
+function promiseFunction1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Function 1 executed via help of promises. .!.");
+            resolve();
+        }, 1000);
+    });
+}
+
+function promiseFunction2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Function 2 executed via help of promises...!.");
+            resolve();
+        }, 1000);
+    });
+}
+
+function promiseFunction3() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Function 3 executed via help of promises..!.");
+            resolve();
+        }, 1000);
+    });
+}
+
+function startPromiseFunctions() {
+    promiseFunction1()
+        .then(promiseFunction2)
+        .then(promiseFunction3)
+        .then(() => {
+            console.log("All functions executed via help of promises...!.");
+        })
+        .catch((error) => {
+            console.error("An error occurred:", error);
+        });
+}
+
+startPromiseFunctions();
