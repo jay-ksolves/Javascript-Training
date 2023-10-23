80f8641603ef905dbbb28fea129d3c178e32de90
jay-ksloves <jay.prakash@ksolves.com>
2023-10-23 13:10:12 +0530

    done fileSystem in node js

---
+++ nodejs/myfirst.js
@@ -0,0 +1,61 @@
+console.log("hello node js!!");
+var fs = require("fs");
+
+//creating a new txt file---------------------------------
+fs.appendFile(
+  "myfile.txt",
+  "\rHello, this the the first file i created,(creating a new file).\r",
+  function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  }
+);
+
+//---reading the file--------
+var reading_txt_file = fs.readFileSync("myfile.txt", function (err) {
+  if (err) throw err;
+  console.log("text content read form file");
+});
+console.log(reading_txt_file.toString());
+
+//opening a file
+
+var opening_txt_file = fs.open("myfile.txt", "w", function (err) {
+  if (err) throw err;
+  console.log("file opened");
+});
+
+//writing to a file------
+
+var writing_txt_file = fs.writeFileSync(
+  "myfile.txt",
+  "\rhello, visitor this is the new content(after write operation)\r",
+  function (err) {
+    if (err) throw err;
+    console.log("Write operation done in file....!");
+  }
+);
+
+//updating a file content
+
+var updating_txt_file = fs.appendFileSync(
+  "myfile.txt",
+  "\rthis is data after updating the file content.(file content updated/added).!\r",
+  function (err) {
+    if (err) throw err;
+    console.log("updating file done..!");
+  }
+);
+
+//renaming a file created----------------
+// var renaming_text_file = fs.rename("myfile.txt", "myfile1.txt", function (err) {
+//   if (err) throw err;
+//   console.log("file renamed successfully..!");
+// });
+
+// deleting the file content---------------
+
+// var deleting_text_file = fs.unlinkSync("myfile1.txt", function (err) {
+//   if (err) throw err;
+//   console.log("file deleted successfully");
+// });
