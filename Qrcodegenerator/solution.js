7c34a930b0112e1b339071e31bec25e58a469af0
jay-ksloves <jay.prakash@ksolves.com>
2023-10-23 21:45:28 +0530

    made a QR code generator using Node npm

--- Qrcodegenerator/solution.js
+++ Qrcodegenerator/solution.js
@@ -10,13 +10,14 @@ inquirer
     },
   ])
   .then((answers) => {
+    console.log(answers);
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
     fs.writeFile("URL.txt", url, (err) => {
       if (err) throw err;
-      console.log("The file has been saved!");
+      console.log("Your Qr code was generated.!");
     });
   })
   .catch((error) => {
